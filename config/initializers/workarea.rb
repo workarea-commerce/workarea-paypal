51687e098ad73ff45913cc6aca11d4adc9db7400
Ben Crouse <bcrouse@workarea.com>
2020-05-26 08:56:10 -0400

    Update Paypal BN code

--- config/initializers/workarea.rb
+++ config/initializers/workarea.rb
@@ -1,7 +1,7 @@
 Workarea.config.tender_types << :paypal
 
 # Gateway application_id is sent as PayPal BN code for revenue sharing
-# 'WebLinc_SP' should be used for all WebLinc installations
-ActiveMerchant::Billing::PaypalGateway.application_id = 'WebLinc_SP'
+# 'Workarea_SP' should be used for all WebLinc installations
+ActiveMerchant::Billing::PaypalGateway.application_id = 'Workarea_SP'
 
 Workarea::Paypal.auto_configure_gateway
