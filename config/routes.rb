eac3c6ea3cbcdab3f82a6ea32d28f3a836554164
Ben Crouse <bcrouse@workarea.com>
2020-06-16 14:17:41 -0400

    Scope routes to include locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Workarea::Storefront::Engine.routes.draw do
-  post 'paypal' => 'paypal#create'
-  put 'paypal/:id/approved' => 'paypal#update', as: :paypal_approved
-  post 'paypal/event' => 'paypal#event', as: :paypal_event
+  scope '(:locale)', constraints: Workarea::I18n.routes_constraint do
+    post 'paypal' => 'paypal#create'
+    put 'paypal/:id/approved' => 'paypal#update', as: :paypal_approved
+    post 'paypal/event' => 'paypal#event', as: :paypal_event
+  end
 end
